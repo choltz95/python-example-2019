43f0457edde84522087995c54b9639ab9e42434a
Chester Holtz <chesterholtz@gmail.com>
2019-08-25 13:43:39 -0700

    Update get_sepsis_score.py

--- get_sepsis_score.py
+++ get_sepsis_score.py
@@ -26,31 +26,31 @@ def move_to_cuda(input_data):
         return pr
 
 def get_sepsis_score(data, model):
-        varofint = ['HR','O2Sat','Temp','SBP','MAP','DBP', 'Resp']
-        d = pd.DataFrame(data=data_mat[:,0:7], columns=varofint)
-        for i, _ in enumerate(varofint):
-            if d[col].count() == 0:
-                d[col] = d[col].fillna(0)
-            elif d[col].count() == 1:
-                d[col] = d[col].fillna(d[col].mean())
-            elif d[col].count() <= 3 :
-                d[col] = d[col].interpolate(method='linear').interpolate(method='nearest')
+    varofint = ['HR','O2Sat','Temp','SBP','MAP','DBP', 'Resp']
+    d = pd.DataFrame(data=data_mat[:,0:7], columns=varofint)
+    for i, _ in enumerate(varofint):
+        if d[col].count() == 0:
+            d[col] = d[col].fillna(0)
+        elif d[col].count() == 1:
+            d[col] = d[col].fillna(d[col].mean())
+        elif d[col].count() <= 3 :
+            d[col] = d[col].interpolate(method='linear').interpolate(method='nearest')
 
-            if d[col].count() > 3:
-                d[col] = d[col].interpolate(method='spline', order=3, axis=0).interpolate(method='nearest')
-                d[col] = np.log(1+d[col])
-                d[col] = (d[col] - d[col].mean())/d[col].std(ddof=0)
-                d[col] = d[col].fillna(0.0)
+        if d[col].count() > 3:
+            d[col] = d[col].interpolate(method='spline', order=3, axis=0).interpolate(method='nearest')
+            d[col] = np.log(1+d[col])
+            d[col] = (d[col] - d[col].mean())/d[col].std(ddof=0)
+            d[col] = d[col].fillna(0.0)
 
-        vital_features = torch.FloatTensor(d.values)
-        input_data = [vital_features.unsqueeze(0), [], []]
+    vital_features = torch.FloatTensor(d.values)
+    input_data = [vital_features.unsqueeze(0), [], []]
 
-        input_data_ = move_to_cuda(input_data)
-        labels_ = labels.cuda()
-        output = model.predict(input_data_)   
-        output = output.tolist()   
-        score = [o[0] for o in output][-1]
-        label = postprocess(output)[-1]
+    input_data_ = move_to_cuda(input_data)
+    labels_ = labels.cuda()
+    output = model.predict(input_data_)   
+    output = output.tolist()   
+    score = [o[0] for o in output][-1]
+    label = postprocess(output)[-1]
 
     return score, label
 
